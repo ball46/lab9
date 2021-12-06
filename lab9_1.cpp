c310bc98c36fa62ac1306b6e337a540607b42867
charnchol <khumball46@gmail.com>
2021-12-06 09:38:16 +0700

    lab 9-1 complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -28,12 +28,4 @@ int main()
     cout << "You have received very KAK items.\n";
   }
   return 0 ;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
-  return 0;
 }
